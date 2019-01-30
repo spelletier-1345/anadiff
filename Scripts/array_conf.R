311b62b40d7c75651aa2d5e60861bf7507619eda
Sandra Pelletier <sandra.pelletier@inra.fr>
2019-01-30 08:54:03 +0100

    Renommage des fichiers : data -> array

--- Scripts/array_conf.R
+++ Scripts/array_conf.R
@@ -9,9 +9,9 @@ conf0 <- c()
 
 conf$dataTest <- "/home/spelletier/Documents/Projets/agilent-irhs/Data/"
 
-conf$version <- "AnaDiff_Script_functions_v4.1.R" # TODO
+conf$version <- "AnaDiff_Script_functions_v5.R" # TODO
 conf$adresse <- .adresse(conf$dataTest)
-conf0$localOpt <- options() ; options(warn=-1)
+conf$localOpt <- options() ; options(warn=-1)
 conf$pck <- c("limma", "httr", "jsonlite")
 conf$verifPackage <- .verifPackages(conf$pck)!=0
 conf$folderArray <- .selectionDossier()
