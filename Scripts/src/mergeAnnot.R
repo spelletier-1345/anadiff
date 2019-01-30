f3a1330c189c89cc503daafde0d0e303f16b32e2
Sandra Pelletier <sandra.pelletier@inra.fr>
2019-01-30 18:13:59 +0100

    Ajout mergeAnnot

---
+++ Scripts/src/mergeAnnot.R
@@ -0,0 +1,18 @@
+###############################################
+# AnaDiff Agilent
+# Le 30 janvier 2018 - Sandra PELLETIER
+###############################################
+
+.mergeAnnot <- function(annotations, tabResult, adresse, senseStep, dataTest=conf$dataTest) {
+  # cree une liste de vecteur de code couleur hexadecimal pour chaque gamme
+  # Args:
+  # Returns:
+  if (!is.null(dataTest)) {print("mergeAnnot")}
+  adresseAnnot <- paste(adresse, annotations, sep="")
+  tabAnnot     <- read.csv(file = adresseAnnot, sep="\t", header=T, encoding="utf-8", check.names = F, as.is = T)
+  if (senseStep=="antisens") {colnames(tabAnnot)[1:2] <- c("probe_rev", "probe_id")}
+  tabFinal     <- merge(tabResult,tabAnnot,by="probe_id",all.y=T)
+  colnames(tabAnnot)[1:2] <- c("probe.x", "probe.y")
+  assign("annot", tabAnnot[,1:2], envir=globalenv())
+  return(tabFinal)
+}
