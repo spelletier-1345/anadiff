c43976a6a289b9b342b5e6bce5d9adbbf989f94c
Sandra Pelletier <sandra.pelletier@inra.fr>
2019-03-28 11:59:47 +0100

    html decimal 0.00

--- Scripts/compil/colorFunctions.R
+++ Scripts/compil/colorFunctions.R
@@ -9,7 +9,7 @@
 .transformBh <- function(bh,
                          gammeBleue=.gammeCouleurs()$bleue,
                          valeurZero=.gammeCouleurs()$noire) {
-  if      (is.na(bh))    {cBh <- "#a0a0a0"}
+  if      (is.na(bh))    {cBh <- "#5E5E5E"}
   else if (bh < 0.00001) {cBh <- gammeBleue[4]}
   else if (bh < 0.01)    {cBh <- gammeBleue[3]}
   else if (bh < 0.05)    {cBh <- gammeBleue[2]}
@@ -20,7 +20,7 @@
 .transformInt <- function(int,
                           gammeJaune=.gammeCouleurs()$jaune,
                           valeurZero=.gammeCouleurs()$noire) {
-  if (is.na(int)) {cInt <- "#a0a0a0"}
+  if (is.na(int)) {cInt <- "#5E5E5E"}
   else {
     int <- round(int)
     int[which(int<0)] <- 0 ; int[which(int>9)] <- 9
@@ -32,7 +32,7 @@
 .transformPval <- function(pval,
                            gammeBleue=.gammeCouleurs()$bleue,
                            valeurZero=.gammeCouleurs()$noire) {
-  if      (is.na(pval))    {cPval <- "#a0a0a0"}
+  if      (is.na(pval))    {cPval <- "#5E5E5E"}
   else if (pval < 0.00001) {cPval <- gammeBleue[4]}
   else if (pval < 0.001)   {cPval <- gammeBleue[3]}
   else if (pval < 0.01)    {cPval <- gammeBleue[2]}
@@ -41,7 +41,7 @@
   return(cPval)
 }
 .transformRat <- function(rat) {
-  if      (is.na(rat))   {cRat <- "#a0a0a0"}
+  if      (is.na(rat))   {cRat <- "#5E5E5E"}
   else if (rat < (-3))   {cRat <- "#00ff00"}
   else if (rat < (-1.5)) {cRat <- "#00aa00"}
   else if (rat < (-1))   {cRat <- "#006600"}
@@ -58,7 +58,7 @@
 .htmlTag <- function(color) {
   htmlCode <- paste("<td align=\"center\" bgcolor=\"",
                     color,
-                    "\">",
+                    "\"  sdnum=\"4108;0;#'##0.00\">",
                     "</td>",
                     sep = "")
   return(htmlCode)
