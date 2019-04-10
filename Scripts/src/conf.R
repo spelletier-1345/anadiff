9a066f7feb3a25a8dddec65f644ced2146c7ee5d
Sandra Pelletier <sandra.pelletier@inra.fr>
2019-04-10 16:59:56 +0200

    Vérification des chemins de conf

--- Scripts/src/conf.R
+++ Scripts/src/conf.R
@@ -1,11 +1,22 @@
+#!/usr/bin/env Rscript
 ###############################################
 # AnaDiff IRHS
 # Le 20 février 2019 - Sandra PELLETIER
-# 
+#
 # Les données de configuration
-# 
+#
 ###############################################
 
-# adresse <- "http://pegasus-bioinfo.angers-nantes.inra.fr/rscripts/Tools/Agilent/"
-adresse <- source("/home/spelletier/Documents/Projets/anadiff/Scripts/")
-# adresse <- "http://147.99.112.52/rscripts/Tools/Agilent/"
+# Configuration
+# Chemin fonction des ordinateurs
+etc <- list("/etc/anadiff.conf",
+     "/usr/local/etc/anadiff.conf",
+     "~/etc/anadiff.conf",
+     "~/.local/etc/anadiff.conf")
+conf <- FALSE
+for (e in etc) {
+  if (file.exists(e)) {
+    source(e)
+    conf <- TRUE
+  }
+}
