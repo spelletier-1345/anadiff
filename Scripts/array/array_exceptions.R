9d826d13953fb1ecbdef42fd7486691a453a1a7f
Sandra Pelletier <sandra.pelletier@inra.fr>
2019-06-26 15:49:21 +0200

    deplacement des fichiers (tous dans src)

--- Scripts/array/array_exceptions.R
+++ Scripts/array/array_exceptions.R
@@ -6,8 +6,7 @@
 
 # Exceptions Aryane_v2
 if (data$designPuce == "70158" | data$designPuce == "70465") { # Cas particulier pour AryANE_v2
-  source("./src/exceptions/tabSAS_aryane_v2.R")
+  source("./src/tabSAS_aryane_v2.R")
   .tabSASayane(export$annot, probeName=RG$genes$ProbeName, tab_sens, tab_antisens,
                color_sens, color_antisens, statBH, labelling)
 }
-
