8122e37564b7ecafc8808aacb39d5c571d804dbb
Sandra Pelletier <sandra.pelletier@inra.fr>
2020-09-29 15:44:54 +0200

    clean folder

---
+++ scripts/colorFunctions.R
@@ -0,0 +1,97 @@
+.gammeCouleurs <- function() {
+  paletteBleue <- colorRampPalette(c("#0000FF","#79F8F8"))(4)
+  paletteJaune <- colorRampPalette(c("#3A3A00","#C5C500","#E6E600","#FFFF66"))(9)
+  paletteVerte <- colorRampPalette(c("#11391E","#00FF00"))(10)
+  paletteRouge <- colorRampPalette(c("#430000","#FE0000"))(10)
+  paletteNoire <- "#000000"
+  return(list(bleue=paletteBleue, jaune=paletteJaune, verte=paletteVerte, rouge=paletteRouge, noire=paletteNoire))
+}
+.transformBh <- function(bh,
+                         gammeBleue=c("#0000FF", "#2852FC", "#50A5FA", "#79F8F8"),
+                         valeurZero="#000000") {
+  if      (is.na(bh))    {cBh <- "#5E5E5E"}
+  else if (bh < 0.00001) {cBh <- gammeBleue[4]}
+  else if (bh < 0.01)    {cBh <- gammeBleue[3]}
+  else if (bh < 0.05)    {cBh <- gammeBleue[2]}
+  else if (bh < 0.1)     {cBh <- gammeBleue[1]}
+  else                   {cBh <- valeurZero}
+  return(cBh)
+}
+.transformInt <- function(int,
+                          gammeJaune=c("#3A3A00", "#6E6E00", "#A2A200", "#C9C900",
+                                       "#D5D500", "#E1E100", "#ECEC19", "#F5F53F", "#FFFF66"),
+                          valeurZero="#000000") {
+  if (is.na(int)) {cInt <- "#5E5E5E"}
+  else {
+    int <- round(int)
+    int[which(int<0)] <- 0 ; int[which(int>9)] <- 9
+    cInt <- ""
+    ifelse (int==0, cInt <- valeurZero , cInt <- gammeJaune[int])
+  }
+  return(cInt)
+}
+.transformPval <- function(pval,
+                           gammeBleue=c("#0000FF", "#2852FC", "#50A5FA", "#79F8F8"),
+                           valeurZero="#000000") {
+  if      (is.na(pval))    {cPval <- "#5E5E5E"}
+  else if (pval < 0.00001) {cPval <- gammeBleue[4]}
+  else if (pval < 0.001)   {cPval <- gammeBleue[3]}
+  else if (pval < 0.01)    {cPval <- gammeBleue[2]}
+  else if (pval < 0.05)    {cPval <- gammeBleue[1]}
+  else                     {cPval <- valeurZero}
+  return(cPval)
+}
+.transformRat <- function(rat) {
+  if      (is.na(rat))   {cRat <- "#5E5E5E"}
+  else if (rat < (-3))   {cRat <- "#00ff00"}
+  else if (rat < (-1.5)) {cRat <- "#00aa00"}
+  else if (rat < (-1))   {cRat <- "#006600"}
+  else if (rat < (-0.5)) {cRat <- "#003300"}
+  else if (rat < 0)      {cRat <- "#001100"}
+  else if (rat == 0)     {cRat <- "#000000"}
+  else if (rat < 0.5)    {cRat <- "#110000"}
+  else if (rat < 1)      {cRat <- "#330000"}
+  else if (rat < 1.5)    {cRat <- "#770000"}
+  else if (rat < 3)      {cRat <- "#aa0000"}
+  else                   {cRat <- "#ff0000"}
+  return(cRat)
+}
+.htmlTag <- function(color) {
+  htmlCode <- paste("<td align=\"center\" bgcolor=\"",
+                    color,
+                    "\"  sdnum=\"4108;0;#'##0.00\">",
+                    "</td>",
+                    sep = "")
+  return(htmlCode)
+}
+.typeOfColor <- function(listArgs, ent) {
+  for (i in listArgs[3:length(listArgs)]) {
+    if (length(grep(i, ent)==1)) {return(names(which(listArgs[3:length(listArgs)]==i)))}
+  }
+}
+.colorCol <- function(ent, val, toc) {
+  # data <- list("ent"=colnames(tab)[i], val=list(tab[,i])[[1]])
+  # toc <- typeOfColor(listArgs, data$ent)
+  if (is.null(toc)) {
+    return("<td></td>")
+  } else if (toc=="ratio") {
+    val <- sapply(val, .transformRat)
+    val <- sapply(val, .htmlTag)
+    return(val)
+  } else if (toc=="pval") {
+    val <- sapply(val, .transformPval)
+    val <- sapply(val, .htmlTag)
+    return(val)
+  } else if (toc=="bh") {
+    val <- sapply(val, .transformBh)
+    val <- sapply(val, .htmlTag)
+    return(val)
+  } else if (toc=="intensity") {
+    val <- sapply(val, .transformInt)
+    val <- sapply(val, .htmlTag)
+    return(val)
+  } else if (toc=="minimize") {
+    val <- "<td align=\"center\" sdnum=\"4108;0;0.00\"></td>"
+    return(val)
+  }
+}
---
+++ scripts/colorisation.R
@@ -0,0 +1,63 @@
+#!/usr/bin/env Rscript
+# @ags-1345-209:PRODUCTION$ ./AnaDiff_IRHS/Scripts/compil/colorisation.R -f ./DataTest/RNAseq_seb/compil.txt -m BH_* -p pval_* -r *_logFC
+# setwd("~/Documents/AnaDiff_IRHS") # Mac
+source("./Scripts/src/conf.R")
+source("./Scripts/compil/colorFunctions.R")
+source("./Scripts/src/colorisation_args.R")
+source("./Scripts/src/readArgs.R")
+
+# Liste des arguments (fichier et entêtes de colonnes)
+listArgs <- list("arguments" = commandArgs(trailingOnly=TRUE))
+listArgs <- list("arguments" = c(
+  "-f", "../Compil/RNAseq/result_anaDiff_compil.txt",
+  "-r", "*_FC",
+  "-p", "*_pval",
+  "-b", "*_BH"))
+
+listArgs <- .readArgs(listArgs,
+                      "./Scripts/help/colorisation_help.R",
+                      "./Scripts/src/colorisation_args.R")
+print(listArgs)
+
+# Lecture du fichier
+cat("--- Lecture du fichier\n")
+tab <- read.table(listArgs$file2color, header=T, sep="\t", encoding="utf-8")
+nbcol <- ncol(tab)
+nbrow <- nrow(tab)
+cat(paste("Nombre de lignes :", nbrow))
+cat(paste("\nNombre de colonnes :", nbcol))
+cat("\nEntêtes des colonnes :")
+cat(paste("\n  ", colnames(tab)))
+cat("\n")
+
+.matColor <- matrix(nrow=nbrow+3, ncol=1)
+.matColor[1,1] <- "<table>"
+.matColor[2,1] <- "<tr></tr>"
+.matColor[3:(nrow(.matColor)-1),1] <- "<tr>"
+.matColor[nrow(.matColor),1] <- "</table>"
+temp <- .matColor[3:(nrow(.matColor)-1),1]
+
+for (i in seq(1:ncol(tab))){
+  data <- list("ent"=colnames(tab)[i], val=list(tab[,i])[[1]])
+  toc <- .typeOfColor(listArgs, data$ent)
+  if (is.null(toc)) {
+    temp <- paste(temp, "<td></td>", sep="")
+  } else {
+    if (toc=="ratio") {
+      val <- sapply(data$val, .transformRat)
+    } else if (toc=="pval") {
+      val <- sapply(data$val, .transformPval)
+    } else if (toc=="bh") {
+      val <- sapply(data$val, .transformBh)
+    } else if (toc=="intensity") {
+      val <- sapply(data$val, .transformInt)
+    }
+    val <- sapply(val, .htmlTag)
+    temp <- paste(temp, val, sep="")
+  }
+}
+.matColor[3:(nrow(.matColor)-1),1] <- paste(temp, "</tr>", sep="")
+rm(i, toc, val, temp, nbcol, nbrow, data, tab)
+
+outFileName <- paste(tools::file_path_sans_ext(listArgs$file2color), ".html", sep="")
+write.table(.matColor, outFileName, row.names=F, col.names=F, quote=F)
