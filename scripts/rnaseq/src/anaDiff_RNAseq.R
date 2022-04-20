fdf06fa5bbf858b501d81e59acc216c6e3322b1f
Sandra Pelletier <sandra.pelletier@inra.fr>
2022-04-20 16:23:29 +0200

    update inrae mail

--- scripts/rnaseq/src/anaDiff_RNAseq.R
+++ scripts/rnaseq/src/anaDiff_RNAseq.R
@@ -10,20 +10,31 @@ anaDiff_RNAseq <- function() {
   localOpt <- options()
   options(warn=-1)
   tryCatch(anaDiff <- .anaDiffRNAseq(),
-           error = function(e) {
-             options(localOpt); warning("anaDiffRNAseq error")
-             print("An error has occurred. Please check your summary file.")
-             print("If you use reads count files produced by a tool that does not come from the IRHS BioInfo team, please contact the team by email (<contact-bioinfo-irhs@inra.fr>) specifying the error next :");
-             print("")
-             print(e);
-           })
+          error = function(e) {
+            options(localOpt); warning("anaDiffRNAseq error")
+            print("An error has occurred. Please check your summary file.")
+            print(
+              "If you use reads count files produced by a tool that does not
+              come from the IRHS BIDefI team, please contact the team by email
+              (<contact-bioinfo-irhs@inrae.fr>) specifying the error next :"
+            );
+            print("")
+            print(e);
+          })
   if (anaDiff == "error") {
-    cat("\n\n#################################
-        \n###   Analyses interrompues   ###
-        \n#################################
-        \nAu moins un fichier est manquant.\nMerci de vérifier votre fichier summary.")
+    cat(
+      "\n\n#################################
+      \n###   Analyses interrompues   ###
+      \n#################################
+      \nAu moins un fichier est manquant.
+      \nMerci de vérifier votre fichier summary."
+    )
   } else {
-    cat("\n\n################################\n ###   Analyses terminees   ###\n################################\n\n")
+    cat(
+      "\n\n################################
+      \n ###   Analyses terminees   ###
+      \n################################\n\n"
+    )
   }
   options(localOpt)
   return("done")
