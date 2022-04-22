5460d2bb1124f28b6c09331956cec16f201697dc
Sandra Pelletier <sandra.pelletier@inra.fr>
2022-04-22 15:41:29 +0200

    update files with the licence

--- scripts/rnaseq/src/mkfileOut.R
+++ scripts/rnaseq/src/mkfileOut.R
@@ -1,3 +1,18 @@
+#!/usr/bin/env Rscript
+
+# Copyright Sandra PELLETIER ([2022-04-20])
+# 
+# [sandra.pelletier@inrae.fr]
+# 
+# This software is governed by the CeCILL license under French law and
+# abiding by the rules of distribution of free software.  You can  use, 
+# modify and/ or redistribute the software under the terms of the CeCILL
+# license as circulated by CEA, CNRS and INRIA at the following URL
+# "http://www.cecill.info". 
+# 
+# The fact that you are presently reading this means that you have had
+# knowledge of the CeCILL license and that you accept its terms.
+
 source("./src/writeLineOut.R")
 
 .mkfileOut <- function(dirName) {
@@ -11,10 +26,9 @@ source("./src/writeLineOut.R")
   file.create(fileOut)
   .writeLineOut("\n###  Analyses differentielles RNAseq ###\n", fileOut)
   .writeLineOut(paste("Le",format(Sys.Date(), "%a %d %b %Y")), fileOut)
-  .writeLineOut("Script Anadiff_RNAseq.R mis a jour le 17/09/2018", fileOut)
-  .writeLineOut("Script Functions_RNAseq.R mis a jour le 17/11/2014", fileOut)
+  .writeLineOut("Script Anadiff_RNAseq.R mis a jour le DD-MM-YYYY", fileOut)
   .writeLineOut("Package R utilises : edgeR, DESeq2, geneplotter", fileOut)
   .writeLineOut(paste("Repertoire de travail :",getwd()), fileOut)
   .writeLineOut(paste("Dossier d'export : ", dirName, sep=""), fileOut)
   return(fileOut)
-} 
+}
